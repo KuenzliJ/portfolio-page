11577fedc24337dc66935c744326a9e02b0c8ca7
Joel <joelkuenzli@bluewin.ch>
2023-09-25 11:21:31 +0200

    Some simple try outs

--- portfolioPage/src/App.vue
+++ portfolioPage/src/App.vue
@@ -1,12 +1,34 @@
-<script setup lang="ts">
-import { defineComponent } from 'vue';
-</script>
-
 <template>
-<div class="app">
-  hello
-  </div>
-</template>
+  <div class="App">
+    <p>{{ name }} - {{ age }}</p>
+    <button @click="changeName('Zelda')">change name</button>
+    <button @click="changeAge(30)">change name</button>
+
+    </div>
+  </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent ({
+  name: 'App',
+  components: {},
+  data() {
+    return {
+      name: 'Link',
+      age: 25 as number | string
+    }
+  },
+  methods: {
+    changeName(name: string){
+      this.name = name
+    },
+    changeAge(age: number | number) {
+      this.age = age
+    }
+  }
+});
+</script>
 
 <style>
 
