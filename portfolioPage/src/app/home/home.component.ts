135ddf4009dc89708b18b46ed0dc3ede8009707a
Joel <joelkuenzli@bluewin.ch>
2023-10-04 08:58:38 +0200

    CSS angepasst

--- portfolioPage/src/app/home/home.component.ts
+++ portfolioPage/src/app/home/home.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  displayText = ""; // Starte mit einem leeren Text
 
+  ngOnInit(): void {
+    this.startTextLoop();
+  }
+
+  startTextLoop() {
+    const text = "Joël Künzli ";
+    let index = 0;
+
+    const loop = () => {
+      setTimeout(() => {
+        this.displayText += text[index];
+        index = (index + 1) % text.length;
+        if (index === 0) {
+          this.displayText = ""; // Zurücksetzen, wenn das Wort vollständig angezeigt wurde
+        }
+        loop();
+      }, 500); // Verzögerung zwischen Buchstaben
+    };
+
+    loop();
+  }
 }
